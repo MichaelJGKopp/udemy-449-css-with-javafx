e0accc05449a7c4fb99473e6021242afb1cb937d
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-11-22 20:49:16 +0100

    use a differen theme from the default

--- src/main/java/dev/lpa/udemy449csswithjavafx/HelloApplication.java
+++ src/main/java/dev/lpa/udemy449csswithjavafx/HelloApplication.java
@@ -11,7 +11,9 @@ public class HelloApplication extends Application {
 
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+    FXMLLoader fxmlLoader =
+        new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+    setUserAgentStylesheet(STYLESHEET_CASPIAN);
     Scene scene = new Scene(fxmlLoader.load(), 600, 275);
     stage.setTitle("Hello!");
     stage.setScene(scene);
