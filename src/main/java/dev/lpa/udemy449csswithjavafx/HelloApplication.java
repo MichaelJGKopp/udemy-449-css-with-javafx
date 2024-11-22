74681b2b9ae6f6a96ce72dc369d8486dda9b20a9
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-11-22 21:03:58 +0100

    more complex style sheet with chatgpt

--- src/main/java/dev/lpa/udemy449csswithjavafx/HelloApplication.java
+++ src/main/java/dev/lpa/udemy449csswithjavafx/HelloApplication.java
@@ -13,8 +13,11 @@ public class HelloApplication extends Application {
   public void start(Stage stage) throws IOException {
     FXMLLoader fxmlLoader =
         new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-    setUserAgentStylesheet(STYLESHEET_CASPIAN);
+//    setUserAgentStylesheet(STYLESHEET_CASPIAN);
+    setUserAgentStylesheet(STYLESHEET_MODENA);
     Scene scene = new Scene(fxmlLoader.load(), 600, 275);
+    scene.getStylesheets().add(HelloApplication.class.getResource("modena-dark.css")
+        .toExternalForm());
     stage.setTitle("Hello!");
     stage.setScene(scene);
     stage.show();
