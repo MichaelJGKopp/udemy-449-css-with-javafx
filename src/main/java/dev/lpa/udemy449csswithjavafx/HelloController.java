4e6afcab6d65f622d4ac0fbd257a9499177a7f5f
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-11-22 22:33:11 +0100

    enlarge label on hover

--- src/main/java/dev/lpa/udemy449csswithjavafx/HelloController.java
+++ src/main/java/dev/lpa/udemy449csswithjavafx/HelloController.java
@@ -5,11 +5,23 @@ import javafx.scene.control.Label;
 
 public class HelloController {
 
-  @FXML
-  private Label welcomeText;
 
   @FXML
-  protected void onHelloButtonClick() {
-    welcomeText.setText("Welcome to JavaFX Application!");
+  private Label label;
+
+  @FXML
+  protected void initialize() {
+  }
+
+  @FXML
+  public void handleMouseEnter(){
+    label.setScaleX(2.0);
+    label.setScaleY(2.0);
+  }
+
+  @FXML
+  public void handleMouseExit(){
+    label.setScaleX(1.0);
+    label.setScaleY(1.0);
   }
 }
